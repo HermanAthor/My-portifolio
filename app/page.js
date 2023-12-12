6e25c2a549cf14cc787dde811280dcd2f75c3b38
Herman Athor <hermanathorg@outlook.com>
2023-12-12 22:00:08 +0100

    Changed the Hero section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import AboutMe from "@/components/AboutMe";
 import ContactMe from "@/components/ContactMe";
 import Hero from "@/components/Hero";
+import HeroSection from "@/components/HeroSection";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
 import Projects from "@/components/projects";
@@ -8,7 +9,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center gap-5 justify-between ">
+    <main className="relative flex min-h-screen flex-col items-center gap-5 justify-between bg-[#121212] ">
       <div className="fixed bottom-3 left-1/2 transform -translate-x-1/2 z-20">
         <ul className="flex flex-row bg-black text-white p-2 rounded-full gap-4 px-5 font-sans font-semibold text-lg">
           <a href="#about">
@@ -33,7 +34,8 @@ export default function Home() {
           height={100}
         />
       </div>
-      <Hero />
+      <HeroSection />
+      {/* <Hero /> */}
       <AboutMe />
       <Skills />
       <Projects />
